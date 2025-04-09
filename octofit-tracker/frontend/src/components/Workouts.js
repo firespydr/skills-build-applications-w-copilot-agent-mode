12889b4da19fe8a7bf3cad36a963fe857c761f6c
OptimusGlitch <firespydr@gmail.com>
2025-04-09 04:08:11 +0000

    React app completed

---
+++ octofit-tracker/frontend/src/components/Workouts.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+function Workouts() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://ideal-space-waddle-rqpx6v65pw3xvvg-8000.app.github.dev/api/workouts')
+      .then(response => response.json())
+      .then(data => setWorkouts(data));
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="my-4">Workouts</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {workouts.map(workout => (
+            <tr key={workout.id}>
+              <td>{workout.id}</td>
+              <td>{workout.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Workouts;
